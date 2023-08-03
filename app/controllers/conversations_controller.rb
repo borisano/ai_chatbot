03313b47d4d6532aff0993fb3fd85fa37541a73c
Anatolii Borysov <borisano@gmail.com>
2023-08-03 20:36:15 +0100

    Check that user is authenticated

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,4 +1,6 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @conversations = current_user.conversations
     respond_to do |format|
